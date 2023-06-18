53bf3c94c73006e2690d343b69461b649d6bf1fb
eduardgrinberg <eduardgrinberg@gmail.com>
2023-06-18 22:13:00 +0300

    pass device id to BE API

--- tcp-server/detector_client.py
+++ tcp-server/detector_client.py
@@ -4,17 +4,16 @@ from io import BytesIO
 import requests as requests
 import logging
 
+
 class DetectorClient:
     def __init__(self):
         self.detector_url = "http://3.66.152.172:5000/upload"
         # self.detector_url = "http://localhost:5000/upload"
 
-
-    def send_data(self,data: bytearray, device_id):
+    def send_data(self, data: bytearray, device_id):
         logging.info('sending ' + str(device_id))
         file_obj = BytesIO(data)
         files = {'file_data': ('rec.raw', file_obj)}
-        response = requests.post(self.detector_url, files=files)
+        data = {'device_id': device_id}
+        response = requests.post(self.detector_url, files=files, data=data)
         logging.info(response.content)
-
-
